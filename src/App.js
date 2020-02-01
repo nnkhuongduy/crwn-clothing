435f5427ef24eabeeda74e0faab726ebe592abb0
Khuong Duy <nnkhuongduy@gmail.com>
2020-02-01 10:44:24 +0700

    update router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop/hats' component={HatsPage}/>
+      </Switch>
+    </div>
   )
 }
 
